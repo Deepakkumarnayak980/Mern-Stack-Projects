f93b504b8cd5aac80bf458d34219092f9a5ec032
Deepak Kumar nayak <deepakkumarnayak715@gmail.com>
2025-12-22 16:32:42 +0530

    install dotenv package and some code

--- THINKBOARD/backend/src/config/db.js
+++ THINKBOARD/backend/src/config/db.js
@@ -3,12 +3,13 @@ import mognoose from 'mongoose'
 
 export const connectionDB = async () =>{
     try {
-        await mognoose.connect("mongodb+srv://deepakkumarnayak715_db_user:deepak123@cluster0.ahb3jub.mongodb.net/?appName=Cluster0")
+        await mognoose.connect(process.env.MONGO_URL)
 
         console.log("MONGODB CONNECTION SUCCESSFULLY");
         
     } catch (error) {
         console.error('error connecting to MONGODB',error);
+        process.exit(1)
         
     }
 
--- THINKBOARD/backend/src/server.js
+++ THINKBOARD/backend/src/server.js
@@ -1,15 +1,20 @@
 import express from 'express';
 import notesRouter from './router/notesRouter.js'
 import { connectionDB } from './config/db.js';
+import dotenv from 'dotenv'
+
+
+dotenv.config()
 
 const app= express();
+const PORT =process.env.PORT || 5002
 
 app.use("/api/notes",notesRouter)
 
 connectionDB()
 
 app.listen(5001,()=>{
-    console.log(`Servers started on PORT : 5001`);
+    console.log(`Servers started on PORT : ${PORT}`);
     
 })
 
