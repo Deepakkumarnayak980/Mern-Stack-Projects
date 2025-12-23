bfdf615914d0b790c5fa9eceae28ebdb77f80492
Deepak Kumar nayak <deepakkumarnayak715@gmail.com>
2025-12-23 08:31:35 +0530

    create  the Middleware & Rate Limiting using the upstash

--- THINKBOARD/backend/src/server.js
+++ THINKBOARD/backend/src/server.js
@@ -1,16 +1,26 @@
+
+import "dotenv/config";
 import express from 'express';
 import notesRouter from './router/notesRouter.js';
 import { connectionDB } from './config/db.js';
-import dotenv from 'dotenv';
+import ratelimiter from './middleWare/rateLimiter.js';
 
-dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5002;
 
-// ✅ MIDDLEWARE FIRST
-app.use(express.json());
+// ✅ MIDDLEWARE FIRST 
+app.use(express.json()); // this middleware will parse JSON bodies : req.body
 app.use(express.urlencoded({ extended: true }));
+
+app.use(ratelimiter)
+
+// our simple custom middleware
+// app.use((req,res,next) => {
+//     console.log(`req method is ${req.method} and Req url is ${req.url}`);
+//     next()
+    
+// })
 
 // ✅ ROUTES AFTER
 app.use("/api/notes", notesRouter);
