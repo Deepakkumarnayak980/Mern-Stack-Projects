7c1c3f7eca214833740b3451d6503b24c049d972
Deepak Kumar nayak <deepakkumarnayak715@gmail.com>
2025-12-23 08:34:00 +0530

    modify the mongo db  and listen router

--- THINKBOARD/backend/src/server.js
+++ THINKBOARD/backend/src/server.js
@@ -1,33 +1,31 @@
-
 import "dotenv/config";
-import express from 'express';
-import notesRouter from './router/notesRouter.js';
-import { connectionDB } from './config/db.js';
-import ratelimiter from './middleWare/rateLimiter.js';
-
+import express from "express";
+import notesRouter from "./router/notesRouter.js";
+import { connectionDB } from "./config/db.js";
+import ratelimiter from "./middleWare/rateLimiter.js";
 
 const app = express();
 const PORT = process.env.PORT || 5002;
 
-// ✅ MIDDLEWARE FIRST 
+// ✅ MIDDLEWARE FIRST
 app.use(express.json()); // this middleware will parse JSON bodies : req.body
 app.use(express.urlencoded({ extended: true }));
 
-app.use(ratelimiter)
+app.use(ratelimiter);
 
 // our simple custom middleware
 // app.use((req,res,next) => {
 //     console.log(`req method is ${req.method} and Req url is ${req.url}`);
 //     next()
-    
+
 // })
 
 // ✅ ROUTES AFTER
 app.use("/api/notes", notesRouter);
 
 // DB connection
-connectionDB();
-
-app.listen(PORT, () => {
-  console.log(`Server started on PORT : ${PORT}`);
+connectionDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server started on PORT : ${PORT}`);
+  });
 });
