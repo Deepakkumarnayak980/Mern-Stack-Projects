aaad96b9bbed7be5bcd3e9e74d346de1b15a6066
Deepak Kumar nayak <deepakkumarnayak715@gmail.com>
2025-12-30 19:52:18 +0530

    update upstash files

--- THINKBOARD/backend/src/config/upstash.js
+++ THINKBOARD/backend/src/config/upstash.js
@@ -1,10 +1,10 @@
 import { Ratelimit } from "@upstash/ratelimit";
 import { Redis } from "@upstash/redis";
 
-// Rate limit: 5 requests per 10 seconds
+// Rate limit: 10 requests per 2 minutes
 const ratelimit = new Ratelimit({
   redis: Redis.fromEnv(),
-  limiter: Ratelimit.slidingWindow(5, "10 s"),
+  limiter: Ratelimit.slidingWindow(100, "5 m"),
   analytics: true,
   prefix: "ratelimit",
 });
