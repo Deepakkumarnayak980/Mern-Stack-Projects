438799ab4d37c79a37e9241434d267db120d1d7a
Deepak Kumar nayak <deepakkumarnayak715@gmail.com>
2026-01-09 08:26:31 +0530

    create navbar and install react-icons

--- mern-crash-course/frontend/src/App.jsx
+++ mern-crash-course/frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box } from "@chakra-ui/react";
+import { Box,useColorModeValue } from "@chakra-ui/react";
 import { Routes, Route } from "react-router-dom";
 import HomePages from "./pages/HomePages";
 import CreatePage from "./pages/CreatePage";
@@ -7,10 +7,10 @@ import NavBar from "./components/NavBar";
 
 const App = () => {
   return (
-    <Box minH="100vh">
+    <Box minH="100vh" bg={useColorModeValue("gray.100", "gray.900")}>
       <NavBar />
 
-      
+
       <Routes>
         <Route path="/" element={<HomePages />} />
         <Route path="/create" element={<CreatePage />} />
