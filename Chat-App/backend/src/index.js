ed1875a4972c87a66a664752484c613b86f40895
Deepak Kumar nayak <deepakkumarnayak715@gmail.com>
2026-02-07 07:37:28 +0530

    crearte the database and create the model for dababase

--- Chat-App/backend/src/index.js
+++ Chat-App/backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import authRoute from "../src/router/auth.routes.js"
+import { connectDB } from "./lib/db.js"
 
 dotenv.config()
 
@@ -16,4 +17,6 @@ const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
   console.log(`Server is running on port :${PORT}`)
+  connectDB()
 })
+  
