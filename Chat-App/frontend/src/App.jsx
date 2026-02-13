b17896caf46bfb9dffb1d02f2e2e00f20a7c65c6
Deepak Kumar nayak <deepakkumarnayak715@gmail.com>
2026-02-13 18:01:46 +0530

    setup the frontend part

--- Chat-App/frontend/src/App.jsx
+++ Chat-App/frontend/src/App.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import Navbar from "./componemts/Navbar";
+import { Route, Routes } from "react-router-dom";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import Settingpage from "./pages/Settingpage";
+import ProfilePage from "./pages/ProfilePage";
+import HomePage from "./HomePage";
+import { useAuthStore } from "./stores/useAuth.store";
 
 const App = () => {
-  return (
-    <div >
-      
-    </div>
-  )
-}
+  const {authUser,checkAuth}=useAuthStore()
 
-export default App
+  useEffect(() =>{
+    checkAuth()
+  },[checkAuth])
+
+  console.log({authUser});
+  
+  return (
+    <div>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/settings" element={<Settingpage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
